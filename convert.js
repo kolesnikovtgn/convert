7616595aa70a86bc9773aa5e4780f0bd4dad8e13
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2018-12-04 16:22:18 +0300

    Ready replace text to html text

--- convert.js
+++ convert.js
@@ -2,9 +2,6 @@
 let textArea1 = document.getElementById('textArea1');
 let textArea2 = document.getElementById('textArea2');
 
-let elem1 = document.createElement('h1');
-let elem2 = document.createElement('b');
-
 textArea1.oninput = function() {
     let regEx1 = /\#(\w+)\s$/;
     let regEx2 = /\*\*(.*)\*\*/;
@@ -13,13 +10,13 @@ textArea1.oninput = function() {
     let getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
 
     textArea1.value = textArea1.value.replace(regEx1, () => {
-        return textArea1.value.match(getText1)[0];
+        return `<h1>${textArea1.value.match(getText1)[0]}</h1>`;
     });
     textArea1.value = textArea1.value.replace(regEx2, () => {
-        return textArea1.value.match(getText2)[0];
+        return `<b>${textArea1.value.match(getText2)[0]}</b>`;
     });
-    textArea2.value = textArea1.value;
-   // textArea2.appendChild('h1');
+
+    textArea2.innerHTML = textArea1.value;
 };
 
 
