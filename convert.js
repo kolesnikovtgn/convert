5697d94c8723363973c85029b6a176fc734e83d0
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2018-12-05 15:49:35 +0300

    change

--- convert.js
+++ convert.js
@@ -3,20 +3,14 @@ let textArea1 = document.getElementById('textArea1');
 let textArea2 = document.getElementById('textArea2');
 
 const regEx1 = /\#(\w+)\s/g;
-const regEx2 = /\*\*(.*)\*\*/g;
-
-const regEx = /\#(\w+)\s/g | /\*\*(.*)\*\*/g;
+// const regEx2 = /\*\*(.*)\*\*/g;
+const regEx2 = /\*\*(\w+)\*\*/g;
 
 const getText1 = /(?<=\#)(\w+)(?=\s)/;
-const getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
-
+// const getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
+const getText2 = /(?<=\*\*)(\w+)(?=\*\*)/;
 
 // textArea1.oninput = function() {
-//     let regEx1 = /\#(\w+)\s$/;
-//     let regEx2 = /\*\*(.*)\*\*/;
-//
-//     let getText1 = /(?<=\#)(\w+)(?=\s)/;
-//     let getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
 //
 //     textArea1.value = textArea1.value.replace(regEx1, () => {
 //         return `<h1>${textArea1.value.match(getText1)[0]}</h1>`;
@@ -28,20 +22,18 @@ const getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
 //     textArea2.innerHTML = textArea1.value;
 // };
 
+textArea1.oninput = function() {
 
+    textArea2.value = textArea1.value;
 
-textArea1.oninput = function(e) {
-    e.preventDefault();
-
-    textArea2.value = textArea1.value.replace(regEx1, () => {
-        return `<h1>${textArea1.value.match(getText1)[0]}</h1>`;
+    textArea2.value = textArea2.value.replace(regEx1, () => {
+        return `<h1>${textArea2.value.match(getText1)[0]}</h1>`;
+    });
+    textArea2.value = textArea2.value.replace(regEx2, () => {
+        return `<b>${textArea2.value.match(getText2)[0]}</b>`;
     });
 
-    textArea2.value = textArea1.value.replace(regEx2, () => {
-        return `<b>${textArea1.value.match(getText2)[0]}</b>`;
-    });
-
-    textArea2.innerHTML= textArea2.value;
-
+    textArea2.innerHTML = textArea2.value;
 };
+
 
