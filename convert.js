757e22c0f745dbc5acae374916766950088f4e3f
lodoss.kolesnikov <lodoss.kolesnikov@gmail.com>
2018-12-05 23:43:46 +0300

    added

--- convert.js
+++ convert.js
@@ -1,1 +1,19 @@
-console.log(document.getElementsByTagName('textarea').value);
+let textArea1 = document.getElementById('textArea1');
+let textArea2 = document.getElementById('textArea2');
+
+textArea1.oninput = function() {
+    let regEx1 = /\#(\w+)\s$/;
+    let regEx2 = /\*\*(.*)\*\*/;
+
+    let getText1 = /(?<=\#)(\w+)(?=\s)/;
+    let getText2 = /(?<=\*\*)(.*)(?=\*\*)/;
+
+    textArea1.value = textArea1.value.replace(regEx1, () => {
+        return `<h1>${textArea1.value.match(getText1)[0]}</h1>`;
+});
+    textArea1.value = textArea1.value.replace(regEx2, () => {
+        return `<b>${textArea1.value.match(getText2)[0]}</b>`;
+});
+
+    textArea2.innerHTML = textArea1.value;
+};
