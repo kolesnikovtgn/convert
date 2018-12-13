5a22d5d0173b89233229551c0a439dbee9ee8f47
lodoss.kolesnikov <lodoss.kolesnikov@gmail.com>
2018-12-13 22:30:05 +0300

    Ready convert for approve

--- convert.js
+++ convert.js
@@ -1,43 +1,22 @@
- let regEx1 = /#(\w+)/g;
- let regEx2 = /\*\*(\w+)\*\*/g;
-
- let getText1 = /(?<=#)(\w+)(?=\s)/g;
- let getText2 = /(?<=\*\*)(\w+)(?=\*\*)/g;
-
-let regEx = /(#(\w+))|(\*\*(\w+)\*\*)/g;
-let getText = /((?<=#)(\w+)(?=\s))|((?<=\*\*)(\w+)(?=\*\*))/g;
-
+// let regEx1 = /#(\w+)/g;
+// let regEx2 = /\*\*(\w+)\*\*/g;
+//
+// let getText1 = /(?<=#)(\w+)(?=\s)/g;
+// let getText2 = /(?<=\*\*)(\w+)(?=\*\*)/g;
+//
+// let regEx = /(#(\w+))|(\*\*(\w+)\*\*)/g;
+// let getText = /((?<=#)(\w+)(?=\s))|((?<=\*\*)(\w+)(?=\*\*))/g;
 
 let textArea1 = document.getElementById('textArea1');
 let textArea2 = document.getElementById('textArea2');
 
-let textA1;
-let textA2;
+let text;
 
 textArea1.oninput = function() {
 
-  textArea2.innerHTML = textArea1.value;
+  text = textArea1.value.replace(/\*\*.+?\*\*/g, `<b>$&</b>`).replace(/\*\*/g,'');
+  text = text.replace(/#.+?[\s,\n]/g, `<h1>$&</h1>`).replace(/<h1>#/g,'<h1>');
+  text = text.replace(/\n/g, '<br>');
 
-  textA1 = textArea1.value;
-      textA1 = textA1.replace(regEx1, () => {
-          return `<h1>${textA1.match(getText1)}</h1>`;
-      });
-
-  textArea2.innerHTML = textA1;
-
-  // textArea2.innerHTML = textArea2.value.replace(getText1, (item) => {
-  //   return `<h1>${item}</h1>`;
-  // });
-  //
-  // textArea2.innerHTML = textArea2.value.replace(getText2, (item) => {
-  //   return `<b>${item}</b>`;
-  // });
-  // textArea2.value = textArea2.value.replace(regEx1, () => {
-  //   return `<h1>${textArea2.value.match(getText1)[0]}</h1>`;
-  // });
-  //
-  // textArea2.value = textArea2.value.replace(regEx2, () => {
-  //   return `<b>${textArea2.value.match(getText2)[0]}</b>`;
-  // });
-
+  textArea2.innerHTML = text;
 };
